7841cf9cc2bc5144643670ed2b1e37d95b1b6158
Derek <derek141772@gmail.com>
2025-09-19 21:25:37 -0700

    create test database in supabase and display content

--- fantasy/client/src/App.jsx
+++ fantasy/client/src/App.jsx
@@ -5,23 +5,28 @@ function App() {
   const [players, setPlayers] = useState([])
 
   const fetchPlayers = async () => {
-    const { data, error } = await supabase.from('players').select('*')
+    const { data, error } = await supabase.from('test_database').select('*')
     if (error) {
       console.error('Error fetching players:', error)
     } else {
       setPlayers(data)
+      console.log('Fetched players:', data)
     }
   }
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1>Players</h1>
+    <div className="flex flex-col items-center w-7xl mx-auto p-4">
+      <h1 className='mb-6'>Players</h1>
       <button onClick={fetchPlayers}>Fetch Players</button>
-      <ul>
+      <div className='grid grid-cols-5 gap-4 mt-4'>
         {players.map((player) => (
-          <li key={player.id}>{player.name}</li>
+          <div key={player.id} className='flex flex-col items-center border rounded hover:bg-indigo-500'>
+            <p>{player.first_name} {player['Last Name']}</p>
+            <img src={player.Headshot} alt={`${player.first_name} ${player['Last Name']}`} className='hover:-translate-y-1 transition-transform duration-200' />
+          </div>
         ))}
-      </ul>
+      </div>
+
     </div>
   )
 }
